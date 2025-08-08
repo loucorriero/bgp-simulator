b1c77a8e4f75251f3256abd58a169a6d94611594
loucorriero <82907331+loucorriero@users.noreply.github.com>
2025-08-08 09:15:46 -0700

    Create _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+// Custom App component. This simply imports the global styles and
+// renders the page component. Additional providers (e.g. for
+// state management or theming) can be added here.
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
