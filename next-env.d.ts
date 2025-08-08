9f145bf999607bf3bf9ced68120c5cf0114e1b10
loucorriero <82907331+loucorriero@users.noreply.github.com>
2025-08-08 08:33:47 -0700

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file is required by Next.js to enable TypeScript support.
+// It should remain empty except for the reference directives above.
